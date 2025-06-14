6dc0ccfb42b5ca24ac3eede8a279c6b2d2fba011
kike <efzzap@gmail.com>
2025-06-14 21:58:41 +0200

    modificación de Footer y NotFound para prueba

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <h2>Miembros del equipo 9:</h2>
+        <h2>Miembros del equipo:</h2>
         <ul>
-            <li>Francisco Javier Sevillano Izquierdo</li>
-            <li>Alexia Lasheras Cortina</li>
-            <li>Enrique Fernández Zapata</li>
+            <li>Javier García</li>
+            <li>Javier López</li>
+            <li>Javier Martínez</li>
+            <li>Javier Pérez</li>
+            <li>Javier Rodríguez</li>
         </ul>
     </div>
   )
 }
+
+export default Footer
 
 export default Footer
